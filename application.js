7bd04b43299a156ee3aa7324076e5318db77b570
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-08-20 11:36:50 -0700

    working solution, needs refactoring

--- application.js
+++ application.js
@@ -1,24 +1,36 @@
+
 $(document).ready(function() {
   var todoTemplate = $.trim($('#todo_template').html());
-
-  function bindEvents() {
-    // Bind functions which add, remove, and complete todos to the appropriate
-    // elements
-  }
-
-  //Create functions to add, remove and complete todos
-
   
+  $('.add').on('click', bindEvents);
+  $('.todo_list').on('click', '.delete', removeTodo);
+  $('.todo_list').on('click', '.complete', completeTodo);
 
   function buildTodo(todoName) {
-    // Creates an jQueryDOMElement from the todoTemplate.
     var $todo = $(todoTemplate);
-    // Modifies it's text to use the passed in todoName.
     $todo.find('h2').text(todoName);
-    // Returns the jQueryDOMElement to be used elsewhere.
     return $todo;
   }
-  
 
-  bindEvents();
+  function removeTodo() {
+    $(this).closest('div').remove();
+  }
+
+  function completeTodo() {
+    $(this).closest('div').css({'font-style':'italic', 'color':'red'});
+    $(this).closest('div').find('h2').append(" - Completed");
+  }
+  
+  function clearInput() {
+    $('.todo').val("");
+    $('.todo').focus();
+  }
+
+  function bindEvents() {
+    todo = $('.todo').val();
+    $template = buildTodo(todo);
+    $('.todo_list').append($template);
+    clearInput();
+  }
+
 });
